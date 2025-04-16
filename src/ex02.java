66bf1c4ad19c21ea11e63793ea6654e76682689a
Danielly Soares Vilela <d.vilela@academico.ifg.edu.br>
2025-04-15 21:31:01 -0300

    Exemplos de entradas

---
+++ src/ex02.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ex02 {
+    public static void main(String[] args) {
+
+        Scanner leia = new Scanner(System.in);
+        System.out.println("Digite o peso: ");
+        double peso = leia.nextDouble();
+        System.out.println("Peso: "+peso);
+    }
+}
